101e733ef960146d85c4e230561b98d1f246149d
SunshineYG888 <354990607@qq.com>
2016-05-02 23:35:56 +0000

    01 initial commit

---
+++ YGKit/YGKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YGKit
+//
+//  Created by yons on 16/5/2.
+//  Copyright © 2016年 yg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YGKit/YGKit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YGKit
+//
+//  Created by yons on 16/5/2.
+//  Copyright © 2016年 yg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
