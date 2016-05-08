347d9b4633e8720758c543855760db325f7dec8f
SunshineYG888 <354990607@qq.com>
2016-05-08 18:57:29 +0000

    04 添加 NSArray 和 NSDictionary 的分类

---
+++ YGKit/YGKit/YGKit/Extension/NSArray+Extension.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+@interface NSArray (Extension)
+
+@end
+
+@interface NSDictionary (Extension)
+
+@end
---
+++ YGKit/YGKit/YGKit/Extension/NSArray+Extension.m
@@ -0,0 +1,35 @@
+#import "NSArray+Extension.h"
+
+@implementation NSArray (Extension)
+
+- (NSString *)descriptionWithLocale:(id)locale
+{
+    NSMutableString *strM = [NSMutableString stringWithString:@"(\n"];
+    
+    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        [strM appendFormat:@"\t%@,\n", obj];
+    }];
+    
+    [strM appendString:@")"];
+    
+    return strM.copy;
+}
+
+@end
+
+@implementation NSDictionary (Extension)
+
+- (NSString *)descriptionWithLocale:(id)locale
+{
+    NSMutableString *strM = [NSMutableString stringWithString:@"{\n"];
+    
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        [strM appendFormat:@"\t%@ = %@;\n", key, obj];
+    }];
+    
+    [strM appendString:@"}\n"];
+    
+    return strM.copy;
+}
+
+@end
